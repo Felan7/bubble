cdc54e5287b2d7be9527cae189dc6d73e5c42e75
Andre Jonas <nipsky@googlemail.com>
2025-01-25 17:33:03 +0100

    Added dynamic music

--- scripts/stargazing.gd
+++ scripts/stargazing.gd
@@ -1,10 +1,14 @@
 extends Node2D
-var resource = load("res://dialogues/stargazing.dialogue")
+var scene_id = "stargazing"
+var resource = load("res://dialogues/" + scene_id + ".dialogue")
 @onready var dialogue = $ExampleBalloon
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DialogueManager.show_example_dialogue_balloon(resource, "start")
+
+	Global.scene_completion_state[scene_id] = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
