2e6918ce8eda3d2f8e6c190626c51b5297c25504
Felan7 <50790924+Felan7@users.noreply.github.com>
2025-01-25 13:51:07 +0100

    Added Dialouge Addon

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,18 +1,14 @@
 extends Node2D
 
 
-signal bubble_enter(bubble_name)
+signal bubble_enter(bubble_name, target)
 signal bubble_leave
-var bubble_name : String = "bubbelig"
-var scene_target : PackedScene = preload("res://scenes/hub.tscn")
-
-
-func change_scene():
-	get_tree().root.add_child(scene_target.instantiate())
+var bubble_name : String = ""
+var scene_target_path : String = "" 
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	bubble_enter.emit(bubble_name)
+	bubble_enter.emit(bubble_name, scene_target_path)
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
