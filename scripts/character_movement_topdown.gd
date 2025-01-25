7a65ccdcd00cf264618285c26df1c6f039c4fe74
Julian S. <J.Schmikale@hotmail.de>
2025-01-25 12:46:16 +0100

    Movement, Eyes-Scene & Glow

--- scripts/character_movement_topdown.gd
+++ scripts/character_movement_topdown.gd
@@ -1,19 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -200.0
-var in_transportation_mode = false
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor() && not in_transportation_mode:
-		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,12 +16,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if in_transportation_mode:
-		if direction_y:
-			velocity.y = direction_y * SPEED
-		else:
-			velocity.y = move_toward(velocity.y, 0, SPEED)
-		
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
