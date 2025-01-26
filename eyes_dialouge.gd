e0414fd78fe14e250e01a63de5d540a052d07ae7
Felan7 <50790924+Felan7@users.noreply.github.com>
2025-01-26 12:49:44 +0100

    Added eyes dialouge scene

---
+++ eyes_dialouge.gd
@@ -0,0 +1,13 @@
+extends Node2D
+var scene_id = "eyes"
+var resource = load("res://dialogues/" + scene_id + ".dialogue")
+var hub_scene : PackedScene = preload("res://scenes/follow_eyes.tscn")
+
+func _ready() -> void:
+	DialogueManager.show_dialogue_balloon(resource, "start")
+	DialogueManager.connect("dialogue_ended", _on_dialouge_ended)
+
+
+func _on_dialouge_ended(resource):
+	# end of scene
+	get_tree().change_scene_to_packed(hub_scene)
