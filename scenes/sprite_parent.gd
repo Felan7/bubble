9edb405a044547ef4adcb5c6be33940b0c4ebbd5
Andre Jonas <nipsky@googlemail.com>
2025-01-26 15:19:12 +0100

    Updated mountain scene

--- scenes/sprite_parent.gd
+++ scenes/sprite_parent.gd
@@ -2,7 +2,13 @@ extends AnimatedSprite2D
 
 signal end_reached
 
-signal position_change(new_position) 
+var step_timer = 0.0
+var step_timer_max = 0.5
+
+signal position_change(new_position)
+
+@onready var step_sound = $StepSound
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x = position.x -400 * delta
@@ -12,5 +18,12 @@ func _process(delta: float) -> void:
 		end_reached.emit()
 		process_mode = PROCESS_MODE_DISABLED
 		
+	step_timer += delta
+
+	if step_timer >= step_timer_max:
+		step_timer = 0.0
+
+		step_sound.play()
 		
+
 	
