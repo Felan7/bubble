f55aabcf9ef7233a34bf7906cd90c248f157f2da
Felan7 <50790924+Felan7@users.noreply.github.com>
2025-01-26 10:39:10 +0100

    Actually use the real dialouge balloon

--- scripts/stargazing.gd
+++ scripts/stargazing.gd
@@ -7,16 +7,12 @@ var hub_scene : PackedScene = preload("res://scenes/M_World.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DialogueManager.show_example_dialogue_balloon(resource, "start")
-
-	Global.scene_completion_state[scene_id] = true
 	DialogueManager.connect("dialogue_ended", _on_dialouge_ended)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
 
 func _on_dialouge_ended(resource):
 	# end of scene
-	print("End of stargazing scene")
-	Global.scene_completion_state["mountain"] = true
+	print("End of " + scene_id + " scene")
+	Global.scene_completion_state["scene_id"] = true
 	get_tree().change_scene_to_packed(hub_scene)
