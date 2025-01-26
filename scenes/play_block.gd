6d71d620194f3930036bc02139ca353feade5099
Andre Jonas <nipsky@googlemail.com>
2025-01-26 14:51:05 +0100

    Fixed physics bug in playroom (hopefully)

--- scenes/play_block.gd
+++ scenes/play_block.gd
@@ -2,10 +2,13 @@ extends RigidBody2D
 
 @onready var drop_sound = $DropSound
 
+func change_position(new_position: Vector2) -> void:
+	global_position = new_position
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body.name)
 	if body.name == "player":
-		global_position = Vector2(540, 300)
+		call_deferred("change_position", Vector2(540, 300))
 	else:
 		var velocity_scaled_volume = linear_velocity.length() / 1000
 		drop_sound.volume_db = -10 + 20 * velocity_scaled_volume
